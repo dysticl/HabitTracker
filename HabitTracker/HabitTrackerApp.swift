bc345f88a21f1c1bcdc8ff3a3ee488f836e9963b
dysticl <dysticl@gmail.com>
2025-05-19 14:00:07 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Daniel Kasanzew on 19.05.25.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitTrackerTests/HabitTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerTests.swift
+//  HabitTrackerTests
+//
+//  Created by Daniel Kasanzew on 19.05.25.
+//
+
+import Testing
+@testable import HabitTracker
+
+struct HabitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
